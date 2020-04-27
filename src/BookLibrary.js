12fcb899695f446e51503adef89743817eaab515
Clinton Onwu <clintononwu@yahoo.com>
2020-04-26 19:08:45 -0600

    React app and directories created

---
+++ src/BookLibrary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function BookLibrary() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default BookLibrary;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BookLibrary from './BookLibrary';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BookLibrary />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
