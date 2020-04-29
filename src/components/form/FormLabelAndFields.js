0df04d584559dfecb0d02f6283cf28262c7f7a60
Clinton Onwu <clintononwu@yahoo.com>
2020-04-28 20:11:42 -0600

    Commit before deleting FormErrors component

--- src/components/form/FormLabelAndFields.js
+++ src/components/form/FormLabelAndFields.js
@@ -11,12 +11,20 @@ class FormLabelAndFields extends React.Component {
                        className={titleError && touched.title? 'invalid':''}
                 />
                 <br />
+                {titleError && touched.title ?
+                    <div className='errorDiv'>{titleError}</div>
+                    : null
+                }
                 <label htmlFor="author">Author:</label>
                 <input type="text" id="author" name='author' onChange={this.props.handleChange}
                        value={this.props.values.author} onBlur={this.props.handleBlur}
                        className={authorError && touched.author? 'invalid':''}
                 />
                 <br />
+                {authorError && touched.author ?
+                    <div className='errorDiv'>{authorError}</div>
+                    : null
+                }
                 <label htmlFor="numPages" >Pages:</label>
                 <input type="number" min="1" max="999999" id="numPages" name='numPages'
                        onChange={this.props.handleChange} value={this.props.values.numPages}
@@ -24,6 +32,10 @@ class FormLabelAndFields extends React.Component {
                        className={numPagesError && touched.numPages? 'invalid':''}
                 />
                 <br />
+                {numPagesError && touched.numPages ?
+                    <div className='errorDiv'>{numPagesError}</div>
+                    : null
+                }
                 <label htmlFor="yearPub" >Year Published:</label>
                 <input type="number" id="yearPub" name='yearPub'
                        onChange={this.props.handleChange} value={this.props.values.yearPub}
@@ -31,6 +43,10 @@ class FormLabelAndFields extends React.Component {
                        className={yearPubError && touched.yearPub? 'invalid':''}
                 />
                 <br />
+                {yearPubError && touched.yearPub ?
+                    <div className='errorDiv'>{yearPubError}</div>
+                    : null
+                }
                 <label htmlFor="bookColor">Book Color</label>
                 <input type="color" id="bookColor" name='bookColor' value={this.props.values.bookColor}
                        onChange={this.props.handleChange}/>
