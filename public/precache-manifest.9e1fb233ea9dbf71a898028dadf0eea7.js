477e9ec7188457f0b898ab9d7cf24d98edc5ba01
Clinton Onwu <clintononwu@yahoo.com>
2020-05-09 08:32:36 -0600

    Added a delete popup  to replace window confirm

---
+++ public/precache-manifest.9e1fb233ea9dbf71a898028dadf0eea7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d5c31b6463781851ea460b39a5d5b9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b4d9edb9be81b508eb08",
+    "url": "./static/js/2.3049df48.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "./static/js/2.3049df48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88f81e564fab92db486d",
+    "url": "./static/js/main.5f190d17.chunk.js"
+  },
+  {
+    "revision": "5b50689c96069830455d",
+    "url": "./static/js/runtime-main.638f81ac.js"
+  },
+  {
+    "revision": "a5caf73188015bbfd03cdcd85d123f52",
+    "url": "./static/media/trash.a5caf731.svg"
+  }
+]);
--- src/components/mainapp/Book.js
+++ src/components/mainapp/Book.js
@@ -3,6 +3,7 @@ import BookDetails from "./BookDetails";
 import BookAction from "./BookAction";
 import {getAndSaveSort} from "./ProvideBooksArray";
 import {moveStuffAround} from "./helper";
+import {animated, useTransition} from "react-spring";
 
 class Book extends React.Component {
     constructor(props) {
@@ -11,17 +12,42 @@ class Book extends React.Component {
         this.handleDelete = this.handleDelete.bind(this);
         this.handleDragStart = this.handleDragStart.bind(this);
         this.handleDragEnter = this.handleDragEnter.bind(this);
+        this.handleConfirmDelete = this.handleConfirmDelete.bind(this);
+        this.handleCancelDelete = this.handleCancelDelete.bind(this);
+        this.state = {
+            confirmDeleteNeeded: false
+        }
     }
     onEditClick() {
         this.props.handleFormOnEdit(this.props.book);
         this.props.handleForm(true, false);
     }
     handleDelete() {
-        if (!window.confirm('Are you sure you want to delete this book?')) return
+        this.setState(
+            {
+                confirmDeleteNeeded: true
+            }
+        )
+    }
+    handleConfirmDelete() {
         this.props.handleDelete(this.props.book.indexForStorage);
         this.props.handleDeletedBook();
+        this.setState(
+            {
+                confirmDeleteNeeded: false
+            }
+        )
+    }
+    handleCancelDelete() {
+        this.setState(
+            {
+                confirmDeleteNeeded: false
+            }
+        )
     }
     handleDragStart(e) {
+        e.preventDefault();
+        if (this.state.confirmDeleteNeeded) return
         e.target.style.opacity = '0.2';
         this.props.setDragElement(e.target.id);
         e.dataTransfer.setData('text',e.target.firstElementChild.firstChild.textContent)
@@ -46,17 +72,53 @@ class Book extends React.Component {
     render() {
         const {book, isFormNeeded} = this.props;
         return (
-            <div id={'book'+book.indexForStorage} className='bookContainer' draggable='true' onDragStart={this.handleDragStart}
-                onDragEnd={this.handleDragEnd} onDragOver={this.handleDragOver} onDrop={this.handleDrop}
+            <div id={'book'+book.indexForStorage} className='bookContainer' onDragStart={this.handleDragStart}
+                 draggable={'true'}
+                 onDragEnd={this.handleDragEnd} onDragOver={this.handleDragOver} onDrop={this.handleDrop}
                  onDragEnter={this.handleDragEnter}
             >
                 <BookDetails book={book}/>
                 <BookAction book={book} onEditClick={this.onEditClick} isFormNeeded={isFormNeeded}
                 handleDelete={this.handleDelete}
                 />
+                <ConfirmDeletePopup confirmDeleteNeeded={this.state.confirmDeleteNeeded}
+                handleConfirmDelete={this.handleConfirmDelete}
+                handleCancelDelete={this.handleCancelDelete} book={book}
+                />
             </div>
         )
     }
+}
+
+function ConfirmDeletePopup(passProps) {
+    const {confirmDeleteNeeded, handleCancelDelete, handleConfirmDelete, book} = passProps;
+    const transitions = useTransition(confirmDeleteNeeded, null, {
+        from: {opacity: 0, marginLeft: window.innerWidth},
+        enter: {opacity: 1, transform: 'scale(1)', width: window.innerWidth, marginLeft: 0},
+        leave: {opacity: 0, width: 0},
+        config: {mass: 1, tension: 250, friction: 50}, //change mass back to 1, tension to 250
+    })
+    return transitions.map(({ item, key, props }) =>
+        item &&
+        <animated.div key={key} style={props} className='confirmDeleteContainer'>
+            <div className='confirmDeleteWrapper'>
+                <div className='formTitle'>
+                    <h2>Are you sure you want to delete this book?</h2>
+                </div>
+                <BookDetails book={book}/>
+                <div className='confirmDeleteButtonContainer'>
+                    <div style={{display: 'inline-block'}}>
+                        <button type='button' id='dontDelete' className='dontDelete' onClick={handleCancelDelete}
+                        >No, Go Back</button>
+                    </div>
+                    <div style={{display: 'inline-block'}}>
+                        <button type="button" id='yesDelete' className='yesDelete' onClick={handleConfirmDelete}
+                        >Yes, Delete</button>
+                    </div>
+                </div>
+            </div>
+        </animated.div>
+    )
 }
 
 export default Book;
